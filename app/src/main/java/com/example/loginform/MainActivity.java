8d899cfa5fd74fc598b439ee4895bb06866c4e43
MrCatDog <failoff.catdog@yandex.ru>
2021-03-12 01:16:02 +0300

    Layout & code chages

--- app/src/main/java/com/example/loginform/MainActivity.java
+++ app/src/main/java/com/example/loginform/MainActivity.java
@@ -9,11 +9,11 @@ import android.widget.EditText;
 
 import com.google.android.material.snackbar.Snackbar;
 
-import java.util.Hashtable;
+import java.util.HashMap;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
-    private final Hashtable<String, String> users = new Hashtable<>();
+    private final HashMap<String, String> users = new HashMap<>();
     private EditText loginEditText;
     private EditText passEditText;
 
@@ -25,40 +25,54 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         passEditText = findViewById(R.id.pass_input);
         Button registerBtn = findViewById(R.id.register_btn);
         Button loginBtn = findViewById(R.id.login_btn);
-        registerBtn.setOnClickListener(this);
-        loginBtn.setOnClickListener(this);
+        registerBtn.setOnClickListener((View v) -> {
+            String login = loginEditText.getText().toString().trim();
+            String pass = passEditText.getText().toString().trim();
+            if (checkOnEmpty(login, pass)) {
+                register(v, login, pass);
+            }
+        });
+        loginBtn.setOnClickListener((View v) -> {
+            String login = loginEditText.getText().toString().trim();
+            String pass = passEditText.getText().toString().trim();
+            if (checkOnEmpty(login, pass)) {
+                login(v, login, pass);
+            }
+        });
         users.put("admin", "12345");
     }
 
-    @Override
-    public void onClick(View v) {
-        String login, pass;
-        if (!(login = loginEditText.getText().toString().trim()).equals("") && !(pass = passEditText.getText().toString().trim()).equals("")) {
-            if (v.getId() == R.id.register_btn) {
-                if (users.containsKey(login)) {
-                    Snackbar.make(v, login + " is already taken.", Snackbar.LENGTH_SHORT).show();
-                } else {
-                    users.put(login, pass);
-                    Snackbar.make(v, login + " is registered.", Snackbar.LENGTH_SHORT).show();
-                }
-            } else {
-                if (users.containsKey(login)) {
-                    if (users.get(login).equals(pass)) {
-                        Snackbar.make(v, "Hello " + login, Snackbar.LENGTH_SHORT).show();
-                    } else {
-                        passEditText.setError("Wrong password!");
-                    }
-                } else {
-                    loginEditText.setError("Wrong login!");
-                }
-            }
-
-        } else {
+    public boolean checkOnEmpty(String login, String pass) {
+        if (login.equals("") || pass.equals("")) {
             if (login.equals("")) {
                 loginEditText.setError("Must be not empty!");
             } else {
                 passEditText.setError("Must be not empty!");
             }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private void register(View v, String login, String pass) {
+        if (users.containsKey(login)) {
+            Snackbar.make(v, login + " is already taken.", Snackbar.LENGTH_SHORT).show();
+        } else {
+            users.put(login, pass);
+            Snackbar.make(v, login + " is registered.", Snackbar.LENGTH_SHORT).show();
+        }
+    }
+
+    private void login(View v, String login, String pass) {
+        if (users.containsKey(login)) {
+            if (users.get(login).equals(pass)) {
+                Snackbar.make(v, "Hello " + login, Snackbar.LENGTH_SHORT).show();
+            } else {
+                passEditText.setError("Wrong password!");
+            }
+        } else {
+            loginEditText.setError("Wrong login!");
         }
     }
 }
